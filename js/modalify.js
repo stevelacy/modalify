c7fdac444f628503bed76dc8662432287c429f09
Steve Lacy <me@slacy.me>
2014-05-21 15:54:05 -0700

    Update to modalify 0.1.0

--- js/modalify.js
+++ js/modalify.js
@@ -1,49 +1,77 @@
+/**
+ * Modalify v0.1.0
+ * http://github.com/stevelacy/modalify.git
+ * Fully responsive, Sleak minimalism Flat design, Modal. 
+ * Copyright 2014 - Steve Lacy me@slacy.me (http://slacy.me) 
+ * License: MIT
+**/
 (function() {
-  $.fn.modalify = function(opts) {
-    return this.each((function(_this) {
-      return function() {
-        var element;
-        if (opts == null) {
-          opts = {};
-        }
-        if (opts.toggle == null) {
-          opts.toggle = 'fadeToggle';
-        }
-        if (opts.speed == null) {
-          opts.speed = 500;
-        }
-        element = _this.data("modalify");
-        $(element).addClass("modalify");
-        if (opts.height) {
-          $(element).css({
-            height: opts.height
+  (function(plugin) {
+    if (typeof define === "function" && define.amd) {
+      define(["jquery"], plugin);
+    } else {
+      plugin(jQuery);
+    }
+  })(function($) {
+    return $.fn.modalify = function(type, opts) {
+      return this.each((function(_this) {
+        return function() {
+          var close, open, options;
+          options = function(opts) {
+            if (opts == null) {
+              opts = {};
+            }
+            if (opts["in"] == null) {
+              opts["in"] = 'fadeIn';
+            }
+            if (opts.out == null) {
+              opts.out = 'fadeOut';
+            }
+            if (opts.speed == null) {
+              opts.speed = 500;
+            }
+            _this.addClass("modalify");
+            if (opts.height) {
+              $(_this).css({
+                height: opts.height
+              });
+            }
+            return opts;
+          };
+          open = function(target, opts) {
+            opts = options(opts);
+            return $(target).stop()[opts["in"]](opts.speed);
+          };
+          close = function(target, opts) {
+            opts = options(opts);
+            return $(target).stop()[opts.out](opts.speed);
+          };
+          if (typeof type === "string") {
+            if (type === "open") {
+              open(_this, opts);
+            }
+            if (type === "close") {
+              close(_this, opts);
+            }
+          } else {
+            opts = type;
+            open(_this, opts);
+          }
+          $(document).keyup(function(e) {
+            if (e.keyCode !== 27) {
+              return;
+            }
+            if (!$(_this).is(":visible")) {
+              return;
+            }
+            return close(_this, opts);
           });
-        }
-        $.fn.toggleModal = function(target, opts) {
-          $(target).html("<div id='closeModal' class='closeModal'>&times;</div>" + ($(target).html()));
-          $("" + target + " #closeModal").click((function(_this) {
-            return function() {
-              return $(target).stop()[opts.toggle](opts.speed);
-            };
-          })(this));
-          return $(target).stop()[opts.toggle](opts.speed);
+          return $(_this).find(".closeModalify").click(function() {
+            return close(_this, opts);
+          });
         };
-        $(_this.click(function() {
-          var target;
-          target = _this.data("modalify");
-          return $(target).toggleModal(target, opts);
-        }));
-        return $(document).keyup(function(e) {
-          if (e.keyCode !== 27) {
-            return;
-          }
-          if (!$(element).is(":visible")) {
-            return;
-          }
-          return $(element).toggleModal(element, opts);
-        });
-      };
-    })(this));
-  };
+      })(this));
+    };
+  });
 
 }).call(this);
